bd20c5187830511f28d112a2333d96bba1bfef1d
Dr2n <darrenfu@outlook.com>
2015-12-12 00:22:06 +1000

    Revert "I don't even know what's in this commit man..."
    This reverts commit 88c96a5b97f550fbc10f280ba247bf807b0e62f4.

--- static/script/chatterClient.js
+++ static/script/chatterClient.js
@@ -2,11 +2,12 @@ window.addEventListener("load", startChatter, false);
 
 function startChatter(){
 	window.param = {
-		username: "DR2N",
-		channel: "Howday"
+		username: "",
+		channel: "Open"
 	};
 
-	window.param. username = prompt('name');
+	window.param.username = prompt("What is your name?");
+	//window.channel = prompt("Join a channel!");
 
 	window.socket = io();
 	socket.addEventListener('newMessage', receiveMessage, false);
@@ -14,7 +15,7 @@ function startChatter(){
 
 	window.textEntry = document.getElementById('textEntry');
 	window.sendButton = document.getElementById('sendButton');
-	window.messageList = document.getElementById('messageList');
+	window.messageArea = document.getElementById('messages');
 
 	textEntry.addEventListener('keydown', textKeyHandler, false);
 	sendButton.addEventListener('click', postMessage, false);
@@ -28,12 +29,7 @@ function textKeyHandler(event){
 }
 
 function postMessage(){
-	var messageText = textEntry.value.trim();
-
-	if (!messageText){
-		return;
-	}
-
+	var messageText = textEntry.value;
 	var message = {
 		contents: messageText,
 		sender: param.username,
@@ -47,31 +43,19 @@ function postMessage(){
 }
 
 function receiveMessage(message){
-	var messageElement = document.createElement('li');
-	var messageTextElement = document.createElement('div')
-	messageTextElement.innerHTML = message.contents;
-
-	
-	messageElement.setAttribute('class', 'message');
-	messageTextElement.setAttribute('class', 'messageText');
-
-
+	var messageDiv = document.createElement('div');
+	var lineBreak = document.createElement('br');
+	messageDiv.innerHTML = message.contents;
+	messageDiv.setAttribute('class', 'message');
 
 	if (message.sender == window.param.username){
-
-		messageElement.setAttribute('class', 'message right');
-
+		messageDiv.style.float = 'right';
 	}else{
-		var senderElement = document.createElement('div');
-		senderElement.setAttribute('class', 'messageSender');
-		senderElement.innerHTML = message.sender[0].toUpperCase();
-		senderElement.setAttribute('title', message.sender);
-		messageElement.appendChild(senderElement);
-
+		messageDiv.style.float = 'left';
 	}
 
-	messageElement.appendChild(messageTextElement);
-	messageList.appendChild(messageElement);
+	messageArea.appendChild(messageDiv);
+	messageArea.appendChild(lineBreak);
 
-	messageList.scrollTop = messageList.scrollHeight;
+	messageArea.scrollTo(0, messageArea.scrollHeight);
 }
